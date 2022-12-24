dc31e0f60de72d355b1a9e24c941ef2278339cc3
Pooja123-sharma <117913332+Pooja123-sharma@users.noreply.github.com>
2022-12-24 18:30:02 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-s4juuj
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-s4juuj)
---
+++ index.js
@@ -0,0 +1,7 @@
+//check if input is divided by 10
+let a=50;
+if ( a % 10 ==0){
+  console .log('divide by 10');
+}else {
+  console.log ('not divided by 10')
+}
